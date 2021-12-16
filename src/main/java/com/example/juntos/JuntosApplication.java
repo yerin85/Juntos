96a7cc8880e249ec69eb87e0ce98125e855948ba
yerin85 <kimy9985@gmail.com>
2021-12-16 22:31:57 +0900

    springboot

---
+++ src/main/java/com/example/juntos/JuntosApplication.java
@@ -0,0 +1,13 @@
+package com.example.juntos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuntosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuntosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/juntos/JuntosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juntos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuntosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
